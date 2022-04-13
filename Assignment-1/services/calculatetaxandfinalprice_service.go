2dfaa6c30aa1c463ed82004594864735aeb89d36
Aditya Pathak <adityapathak@Administrators-MacBook-Pro.local>
2022-04-13 19:06:07 +0530

    types made constant

--- Assignment-1/services/calculatetaxandfinalprice_service.go
+++ Assignment-1/services/calculatetaxandfinalprice_service.go
@@ -1,13 +1,19 @@
 package services
 
+const (
+	raw          = "raw"
+	manufactured = "manufactured"
+	imported     = "imported"
+)
+
 func CalculateTaxAndFinalPrice(t string, p float64, iq int) (tax float64, finalPrice float64) {
 	switch t {
-	case "raw":
+	case raw:
 		tax = (p * 12.5) / 100
-	case "manufactured":
+	case manufactured:
 		tpf := (p * 12.5) / 100
 		tax = tpf + (2*(p+tpf))/100
-	case "imported":
+	case imported:
 		importDuty := (10 * p) / 100
 		finalCost := importDuty + ((p * 12.5) / 100) + p
 		var surchage float64
